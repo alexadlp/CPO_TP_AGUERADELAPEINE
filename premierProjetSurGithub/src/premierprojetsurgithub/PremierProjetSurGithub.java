c2ed9c8bc2b8524bf47c6d13343c7e68f903019d
Alexandre <alexandre.aguera-delapeine@epfedu.fr>
2025-10-22 16:34:56 +0200

    PROJET TP1

--- premierProjetSurGithub/src/premierprojetsurgithub/PremierProjetSurGithub.java
+++ premierProjetSurGithub/src/premierprojetsurgithub/PremierProjetSurGithub.java
@@ -4,6 +4,8 @@
  */
 package premierprojetsurgithub;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alexa
@@ -15,6 +17,33 @@ public class PremierProjetSurGithub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Saisair entier 1 ?");
+        int entier1 = sc.nextInt();
+        System.out.println("Saisair entier 2 ?");
+        int entier2 = sc.nextInt();
+
+        double resultat;
+
+        System.out.println("voici l'addition");
+        resultat = entier1 + entier2;
+        System.out.println("resulat=" + resultat);
+
+        System.out.println(" voici le produit");
+        resultat = entier1 * entier2;
+        System.out.println("Résultat = " + resultat);
+
+        System.out.println("voici la difference");
+        resultat = entier1 - entier2;
+        System.out.println("resulat=" + resultat);
+
+        System.out.println("voici la division");
+        if (entier2 != 0) {
+            resultat = entier1 / entier2;
+            System.out.println("Résultat = " + resultat);
+        
+           
+        }
     }
-    
-}
+    }
